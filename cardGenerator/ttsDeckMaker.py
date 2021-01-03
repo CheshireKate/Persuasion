1cbfc0e9e3a174c275cb74fde3c630729562a82f
Katie Allred <me@cheshirekate.com>
2021-01-03 09:01:59 -0800

    Deck generator!

---
+++ cardGenerator/ttsDeckMaker.py
@@ -0,0 +1,327 @@
+
+traitDescription = 'To resleeve, right click, select State, then pick the relevant sleeve'
+proposalDescription = 'Give one of your own Traits to {} to accept their proposal, then resleeve this card as your own Trait.'
+cardCount = 60
+
+base = {
+    'deckID': 1,
+    'name': 'Desire',
+    'description': 'Win if you marry a player with a sum of symbols matching these!\n\n{}'.format(traitDescription),
+    'face': 'http://cloud-3.steamusercontent.com/ugc/1656728318303212718/D99C13F05CEDC7DEEFFF72A19EA4F8E86EB2EE93/',
+    'back': 'http://cloud-3.steamusercontent.com/ugc/1656728318303082524/9DB5207068847C958622BEC0325573DBA445BC72/'
+}
+
+decks = [
+    {
+        'deckID': 2,
+        'name': 'Black Dog Trait',
+        'description': traitDescription,
+        'face': 'http://cloud-3.steamusercontent.com/ugc/1656728318303213812/E370DBF25C57D71EF792EE78DADF9912E09119AA/',
+        'back': 'http://cloud-3.steamusercontent.com/ugc/1656728318303124139/3386D885EC1630D77C835EA6C49FA6B6C64C8952/'
+    },
+    {
+        'deckID': 3,
+        'name': 'Black Dog Proposal Trait',
+        'description': proposalDescription.format('Black Dog'),
+        'face': 'http://cloud-3.steamusercontent.com/ugc/1656728318303213812/E370DBF25C57D71EF792EE78DADF9912E09119AA/',
+        'back': 'http://cloud-3.steamusercontent.com/ugc/1656728318303095848/0A7AFF3697B60BA7CA97B64DD6C409BE9083F83A/'
+    },
+    {
+        'deckID': 4,
+        'name': 'Blue Lion Trait',
+        'description': traitDescription,
+        'face': 'http://cloud-3.steamusercontent.com/ugc/1656728318303214638/96A9A0B51C7213048D54E199759674AF031AA8BB/',
+        'back': 'http://cloud-3.steamusercontent.com/ugc/1656728318303214207/F6C545671C5D9E26A8587061ADE177DD0240D2B0/'
+    },
+    {
+        'deckID': 5,
+        'name': 'Blue Lion Proposal Trait',
+        'description': proposalDescription.format('Blue Lion'),
+        'face': 'http://cloud-3.steamusercontent.com/ugc/1656728318303214638/96A9A0B51C7213048D54E199759674AF031AA8BB/',
+        'back': 'http://cloud-3.steamusercontent.com/ugc/1656728318303217985/F8CD11616ADF2BD97534E88EC4AACCD547120743/'
+    },
+    {
+        'deckID': 6,
+        'name': 'Green Tree Trait',
+        'description': traitDescription,
+        'face': 'http://cloud-3.steamusercontent.com/ugc/1656728318303220341/67E170C5714CBFD8C441BE303C48F50A1AB3620D/',
+        'back': 'http://cloud-3.steamusercontent.com/ugc/1656728318303219676/E43B426C0EC6E5C4828774496FA767B4D9FD2810/'
+    },
+    {
+        'deckID': 7,
+        'name': 'Green Tree Proposal Trait',
+        'description': proposalDescription.format('Green Tree'),
+        'face': 'http://cloud-3.steamusercontent.com/ugc/1656728318303220341/67E170C5714CBFD8C441BE303C48F50A1AB3620D/',
+        'back': 'http://cloud-3.steamusercontent.com/ugc/1656728318303219997/6B4832110703E9D9AFDA8F0460CE849B90C170BE/'
+    },
+    {
+        'deckID': 8,
+        'name': 'Cyan Shield Trait',
+        'description': traitDescription,
+        'face': 'http://cloud-3.steamusercontent.com/ugc/1656728318303224260/38901D3C8F3D4F324B174E6350DAB216F2E0B620/',
+        'back': 'http://cloud-3.steamusercontent.com/ugc/1656728318303223441/26EBC8436FCDFF59041E702E58506D1E068985AE/'
+    },
+    {
+        'deckID': 9,
+        'name': 'Cyan Shield Proposal Trait',
+        'description': proposalDescription.format('Cyan Shield'),
+        'face': 'http://cloud-3.steamusercontent.com/ugc/1656728318303224260/38901D3C8F3D4F324B174E6350DAB216F2E0B620/',
+        'back': 'http://cloud-3.steamusercontent.com/ugc/1656728318303223836/3806BBC397E987E184F11B5443CE7BEE64EA3527/'
+    },
+    {
+        'deckID': 10,
+        'name': 'Yellow Sun Trait',
+        'description': traitDescription,
+        'face': 'http://cloud-3.steamusercontent.com/ugc/1656728318303226961/032D0B279254B576AB087B7D112570245B19C621/',
+        'back': 'http://cloud-3.steamusercontent.com/ugc/1656728318303226151/8173A6620818B8C3A202227FF9B6198F64A1AA13/'
+    },
+    {
+        'deckID': 11,
+        'name': 'Yellow Sun Proposal Trait',
+        'description': proposalDescription.format('Yellow Sun'),
+        'face': 'http://cloud-3.steamusercontent.com/ugc/1656728318303226961/032D0B279254B576AB087B7D112570245B19C621/',
+        'back': 'http://cloud-3.steamusercontent.com/ugc/1656728318303226537/BD2BBB72ADE5A3E06909B54A1971B9696C389719/'
+    },
+    {
+        'deckID': 12,
+        'name': 'Pink Horn Trait',
+        'description': traitDescription,
+        'face': 'http://cloud-3.steamusercontent.com/ugc/1656728318303221584/A39826327DFB806200ECC059D8B668F7E784C390/',
+        'back': 'http://cloud-3.steamusercontent.com/ugc/1656728318303220938/68722CE40FB18FE8CFAC5D06AB8E00D53BCA4A33/'
+    },
+    {
+        'deckID': 13,
+        'name': 'Pink Horn Proposal Trait',
+        'description': proposalDescription.format('Pink Horn'),
+        'face': 'http://cloud-3.steamusercontent.com/ugc/1656728318303221584/A39826327DFB806200ECC059D8B668F7E784C390/',
+        'back': 'http://cloud-3.steamusercontent.com/ugc/1656728318303221269/266EE39298790D5C9FC6BA07628D5948D45A4C56/'
+    },
+    {
+        'deckID': 14,
+        'name': 'Purple Clover Trait',
+        'description': traitDescription,
+        'face': 'http://cloud-3.steamusercontent.com/ugc/1656728318303222974/4626FF287EFE66E5BF36120DD1CD57FACF0CC354/',
+        'back': 'http://cloud-3.steamusercontent.com/ugc/1656728318303222134/F7EA4278B2C0EC7640592E7804317356071B0788/'
+    },
+    {
+        'deckID': 15,
+        'name': 'Purple Clover Proposal Trait',
+        'description': proposalDescription.format('Purple Clover'),
+        'face': 'http://cloud-3.steamusercontent.com/ugc/1656728318303222974/4626FF287EFE66E5BF36120DD1CD57FACF0CC354/',
+        'back': 'http://cloud-3.steamusercontent.com/ugc/1656728318303222594/156898DB26CD619099155063CE19F4987CD3365C/'
+    },
+    {
+        'deckID': 16,
+        'name': 'White Horse Trait',
+        'description': traitDescription,
+        'face': 'http://cloud-3.steamusercontent.com/ugc/1656728318303225724/1F8FBF3EB8BB2B337A755EC585401D29CF9AA7C3/',
+        'back': 'http://cloud-3.steamusercontent.com/ugc/1656728318303224897/771F94BB2FA229907293E17D9AC8310D6617C6B0/'
+    },
+    {
+        'deckID': 17,
+        'name': 'White Horse Proposal Trait',
+        'description': proposalDescription.format('White Horse'),
+        'face': 'http://cloud-3.steamusercontent.com/ugc/1656728318303225724/1F8FBF3EB8BB2B337A755EC585401D29CF9AA7C3/',
+        'back': 'http://cloud-3.steamusercontent.com/ugc/1656728318303225317/CF5F1D6DD0715981594F7B5B7147442146FB2C63/'
+    }
+]
+
+ttsObject = '''{{
+  "SaveName": "",
+  "GameMode": "",
+  "Date": "",
+  "Gravity": 0.5,
+  "PlayArea": 0.5,
+  "GameType": "",
+  "GameComplexity": "",
+  "Tags": [],
+  "Table": "",
+  "Sky": "",
+  "Note": "",
+  "Rules": "",
+  "TabStates": {{}},
+  "ObjectStates": [
+    {{
+      "Name": "Deck",
+      "Transform": {{
+        "posX": -7.72676373,
+        "posY": 0.999352634,
+        "posZ": 11.263113,
+        "rotX": 2.76832234E-05,
+        "rotY": 180.001541,
+        "rotZ": 180.210648,
+        "scaleX": 1.0,
+        "scaleY": 1.0,
+        "scaleZ": 1.0
+      }},
+      "Nickname": "Traits",
+      "Description": "",
+      "GMNotes": "",
+      "ColorDiffuse": {{
+        "r": 0.713235259,
+        "g": 0.713235259,
+        "b": 0.713235259
+      }},
+      "Locked": false,
+      "Grid": true,
+      "Snap": true,
+      "IgnoreFoW": false,
+      "MeasureMovement": false,
+      "DragSelectable": true,
+      "Autoraise": true,
+      "Sticky": true,
+      "Tooltip": true,
+      "GridProjection": false,
+      "HideWhenFaceDown": true,
+      "Hands": false,
+      "SidewaysCard": false,
+      "DeckIDs": [ 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 139, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159 ],
+      "CustomDeck": {{
+        "1": {{
+          "FaceURL": "http://cloud-3.steamusercontent.com/ugc/1656728318303212718/D99C13F05CEDC7DEEFFF72A19EA4F8E86EB2EE93/",
+          "BackURL": "http://cloud-3.steamusercontent.com/ugc/1656728318303082524/9DB5207068847C958622BEC0325573DBA445BC72/",
+          "NumWidth": 10,
+          "NumHeight": 6,
+          "BackIsHidden": true,
+          "UniqueBack": false,
+          "Type": 0
+        }}
+      }},
+      "LuaScript": "",
+      "LuaScriptState": "",
+      "XmlUI": "",
+      "ContainedObjects": [
+        {}
+      ],
+      "GUID": "100000"
+    }}
+  ],
+  "LuaScript": "",
+  "LuaScriptState": "",
+  "XmlUI": "",
+  "VersionNumber": ""
+}}'''
+
+card = '''{{
+          "Name": "Card",
+          "Transform": {{
+            "posX": -7.69503927,
+            "posY": 1.0400672,
+            "posZ": 11.0366421,
+            "rotX": 359.9547,
+            "rotY": 179.977524,
+            "rotZ": 179.976715,
+            "scaleX": 1.0,
+            "scaleY": 1.0,
+            "scaleZ": 1.0
+          }},
+          "Nickname": "Desire",
+          "Description": "Win if you marry a player with a sum of symbols matching these!",
+          "GMNotes": "",
+          "ColorDiffuse": {{
+            "r": 0.713235259,
+            "g": 0.713235259,
+            "b": 0.713235259
+          }},
+          "Locked": false,
+          "Grid": true,
+          "Snap": true,
+          "IgnoreFoW": false,
+          "MeasureMovement": false,
+          "DragSelectable": true,
+          "Autoraise": true,
+          "Sticky": true,
+          "Tooltip": true,
+          "GridProjection": false,
+          "HideWhenFaceDown": true,
+          "Hands": true,
+          "CardID": "{cardID:d}",
+          "SidewaysCard": false,
+          "CustomDeck": {{
+            "1": {{
+              "FaceURL": "http://cloud-3.steamusercontent.com/ugc/1656728318303088143/1D9D9C7EACE6471DF5C04FF5BBA71D166842D385/",
+              "BackURL": "http://cloud-3.steamusercontent.com/ugc/1656728318303082524/9DB5207068847C958622BEC0325573DBA445BC72/",
+              "NumWidth": 10,
+              "NumHeight": 6,
+              "BackIsHidden": true,
+              "UniqueBack": false,
+              "Type": 0
+            }}
+          }},
+          "LuaScript": "",
+          "LuaScriptState": "",
+          "XmlUI": "",
+          "GUID": "10{cardID:04d}",
+          "States": {{
+          {states}
+          }}
+        }}'''
+
+# States start
+state = '''"{deckID:d}": {{
+              "Name": "Card",
+              "Transform": {{
+                "posX": -5.430677,
+                "posY": 0.982206643,
+                "posZ": 11.5248652,
+                "rotX": 359.9847,
+                "rotY": 180.000015,
+                "rotZ": -0.00292019383,
+                "scaleX": 1.0,
+                "scaleY": 1.0,
+                "scaleZ": 1.0
+              }},
+              "Nickname": "{name}",
+              "Description": "{description}",
+              "GMNotes": "",
+              "ColorDiffuse": {{
+                "r": 0.713235259,
+                "g": 0.713235259,
+                "b": 0.713235259
+              }},
+              "Locked": false,
+              "Grid": true,
+              "Snap": true,
+              "IgnoreFoW": false,
+              "MeasureMovement": false,
+              "DragSelectable": true,
+              "Autoraise": true,
+              "Sticky": true,
+              "Tooltip": true,
+              "GridProjection": false,
+              "HideWhenFaceDown": true,
+              "Hands": true,
+              "CardID": "{cardID:d}",
+              "SidewaysCard": false,
+              "CustomDeck": {{
+                "{deckID:d}": {{
+                  "FaceURL": "{face}",
+                  "BackURL": "{back}",
+                  "NumWidth": 10,
+                  "NumHeight": 6,
+                  "BackIsHidden": true,
+                  "UniqueBack": false,
+                  "Type": 0
+                }}
+              }},
+              "LuaScript": "",
+              "LuaScriptState": "",
+              "XmlUI": "",
+              "GUID": "10{cardID:04d}"
+            }}'''
+
+# Generate combinations
+cards = []
+for cardID in range(cardCount):
+    states = []
+    for deck in decks:
+        deck['cardID'] = deck['deckID'] * 100 + cardID
+        states.append(state.format(**deck))
+
+    base['cardID'] = base['deckID'] * 100 + cardID
+    base['states'] = ','.join(states);
+
+    cards.append(card.format(**base))
+
+with open('allCards.json', 'w') as f:
+    f.write(ttsObject.format(','.join(cards)))
